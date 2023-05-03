1491c755ed70f33128ba192dfd2c9c331d88f156
Leandro Arturi <lea.arturi@gmail.com>
2023-05-03 20:26:36 -0300

    Cors

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from word_forms.word_forms import get_word_forms
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://apirest.com.ar:8900"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/random_word_family/{word}")
 async def random_word_family(word: str):
