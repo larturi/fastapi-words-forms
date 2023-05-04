0c745bf5126870390304476048d9360d02cc6fb1
Leandro Arturi <lea.arturi@gmail.com>
2023-05-03 22:14:33 -0300

    SSL

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import ssl
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from word_forms.word_forms import get_word_forms
+
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain(certfile="certs/cert.crt", keyfile="certs/cert.key")
 
 origins = [
     "http://localhost",
@@ -24,3 +28,7 @@ app.add_middleware(
 async def random_word_family(word: str):
     word_forms = get_word_forms(word)
     return {"word": word, "word_forms": word_forms}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, ssl_keyfile="certs/cert.key", ssl_certfile="certs/cert.crt")
